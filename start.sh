3d312e098a120504ffb0581b470189822e93deb5
Bryan Stockus <bstockus@gmail.com>
2015-04-14 20:00:39 -0500

    Minor Tweak

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /tmp
+
+# try to remove the repo if it already exists
+rm -rf assignments; true
+
+git clone https://github.com/bstockus/assignments.git
+
+cd assignments
+
+npm install
+
+node ./index.js
